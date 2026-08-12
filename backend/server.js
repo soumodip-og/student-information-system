2b4722fa46f32d5fc7c8b61561620997c80602e3
Soumodip Mondal <soumodipmondal1357@gmail.com>
2026-08-12 10:09:28 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ mongoose
         console.log("MongoDB connection error:", error);
     });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
